8fb30911457accd1eb29716e4c6fceb49a25b45f
Shakib <mdshakib771575@gmail.com>
2026-07-14 09:54:09 -0700

    added testimonials section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
       <TopBrands></TopBrands>
       <WhyChooseUs />
       <Statistics />
-      {/* <Testimonials />
-       <Newsletter /> */}
+      <Testimonials />
+       {/* <Newsletter /> */}
     </main>
   );
 }
